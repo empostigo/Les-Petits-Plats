f23c64f1cd648089dfe13444fafea28775c06c64
empostigo <emmanuel.postigo@tutanota.com>
2023-07-08 09:47:52 +0200

    Put overflow in recipe text rathan than entire card

--- scripts/templates/cards.js
+++ scripts/templates/cards.js
@@ -21,10 +21,10 @@ export default class Card {
     return `<div class="col-12 col-md-4 pb-3">
               <div class="card rounded-4">
                 <img src="/assets/recipes/${this.cardInfos.image}" class="card-img-top rounded-top-4 card__img" alt="${this.cardInfos.name}">
-                <div class="card-body card__desc overflow-auto">
+                <div class="card-body card__desc">
                   <h2 class="card-title mt-2 mb-4 card__h2">${this.cardInfos.name}</h2>
                   <h3 class="card__h3 mb-2">RECETTE</h3>
-                  <p class="card__text mb-4">${this.cardInfos.description}</p>
+                  <p class="card__text overflow-auto mb-4">${this.cardInfos.description}</p>
                   <h3 class="card__h3">INGRÉDIENTS</h3>
                   <div class="d-flex flex-wrap">
                     ${this.cardIngredients}
