9a18dfb8dfdad08036c037df0e6d1e5432a85551
empostigo <emmanuel.postigo@tutanota.com>
2023-07-16 19:45:19 +0200

    Add dataset to input to trigger searching

--- scripts/utils/input.js
+++ scripts/utils/input.js
@@ -5,17 +5,22 @@ export default class Input {
   }
 
   static waitForUserEntry(...inputs) {
-    inputs.forEach(input =>
+    inputs.forEach(input => {
       input.inputId.addEventListener("input", content => {
         content.target.value.length > 0
           ? input.inputCross.classList.remove("opacity-0")
           : input.inputCross.classList.add("opacity-0")
-
-        input.inputCross.addEventListener("click", () => {
-          input.inputId.value = ""
-          input.inputCross.classList.add("opacity-0")
-        })
       })
-    )
+
+      input.inputId.addEventListener("input", content => {
+        if (content.target.value.length > 2)
+          content.target.dataset.search = "enable"
+      })
+
+      input.inputCross.addEventListener("click", () => {
+        input.inputId.value = ""
+        input.inputCross.classList.add("opacity-0")
+      })
+    })
   }
 }
