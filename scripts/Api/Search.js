6c8aa7b1cc1977b9a7f1108d83d5bc3967fcb7bd
empostigo <emmanuel.postigo@tutanota.com>
2023-07-19 22:21:29 +0200

    Implement Singleton pattern for Search class

--- scripts/Api/Search.js
+++ scripts/Api/Search.js
@@ -1,4 +1,13 @@
 export default class Search {
+  constructor() {
+    if (Search.exists) return Search.instance
+
+    Search.instance = this
+    Search.exists = true
+
+    return this
+  }
+
   set target(str) {
     this._target = str
     console.log(this._target)
