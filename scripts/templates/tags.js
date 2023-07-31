bbb0d78626a6722e099f8d4a9dbe37284f8ae225
empostigo <emmanuel.postigo@tutanota.com>
2023-07-31 11:34:25 +0200

    Tags rendering ok

--- scripts/templates/tags.js
+++ scripts/templates/tags.js
@@ -5,29 +5,46 @@ export default class Tags {
     this.sibbling = document.getElementById("cardDom")
   }
 
+  static flag = 0
+
+  static sibblingOrigin = 130
+
   displayTag() {
     const tag = document.createElement("div")
-    tag.classList.add(
-      "d-flex",
-      "align-center",
-      "gap-5",
-      "positon-relative",
-      "tags"
-    )
+    tag.classList.add("col", "tags")
+    tag.id = this.tag
+
+    const innerTag = document.createElement("div")
+    innerTag.classList.add("position-relative", "d-flex", "tags__inner")
 
     const tagText = document.createElement("span")
     tagText.textContent = this.tag
-    tagText.classList.add("tags__text")
+    tagText.classList.add("p-3", "tags__text")
 
     const closingTag = document.createElement("img")
     closingTag.src = "/assets/tags/cross.svg"
     closingTag.setAttribute("alt", "Supprimer le tag")
     closingTag.className = "tags__cross"
 
-    tag.append(tagText, closingTag)
+    innerTag.append(tagText, closingTag)
+    tag.append(innerTag)
     this.tagsWrapper.append(tag)
+    Tags.flag++
 
-    const translation = this.sibbling.offsetTop + 54
+    const translation =
+      this.tagsWrapper.offsetHeight + this.tagsWrapper.offsetTop + 44
     this.sibbling.style.top = `${translation}px`
+
+    closingTag.addEventListener("click", () => {
+      if (--Tags.flag === 0) {
+        this.sibbling.style.top = `${Tags.sibblingOrigin}px`
+        console.log(this.sibbling.style.top)
+      } else {
+        const minusTranslation = this.sibbling.offsetTop - tag.offsetHeight
+        this.sibbling.style.top = `${minusTranslation}px`
+      }
+
+      document.getElementById(this.tag).remove()
+    })
   }
 }
