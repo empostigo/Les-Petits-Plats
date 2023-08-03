2cd7220ee0e273aa5149e72b40585841d9284496
empostigo <emmanuel.postigo@tutanota.com>
2023-08-03 23:34:48 +0200

    Finish tag selection

--- scripts/Api/Search.js
+++ scripts/Api/Search.js
@@ -45,6 +45,37 @@ export default class Search {
 
         break
 
+      case "ingredientsTags":
+        this.ingredientsSelect = this.recipeTerms.wholeIngredientsList.filter(
+          ingredient => ingredient === pattern
+        )
+
+        this.appliancesSelect = this.recipeTerms.wholeAppliancesList
+        this.ustensilsSelect = this.recipeTerms.wholeUstensilsList
+
+        break
+
+      case "appliancesTags":
+        this.ingredientsSelect = this.recipeTerms.wholeIngredientsList
+
+        this.appliancesSelect = this.recipeTerms.wholeAppliancesList.filter(
+          appliance => appliance.toLowerCase() === toFind
+        )
+
+        this.ustensilsSelect = this.recipeTerms.wholeUstensilsList
+
+        break
+
+      case "ustensilsTags":
+        this.ingredientsSelect = this.recipeTerms.wholeIngredientsList
+        this.appliancesSelect = this.recipeTerms.wholeAppliancesList
+
+        this.ustensilsSelect = this.recipeTerms.wholeUstensilsList.filter(
+          ustensil => ustensil.toLowerCase() === toFind
+        )
+
+        break
+
       default:
         break
     }
@@ -61,19 +92,19 @@ export default class Search {
     switch (category) {
       case "main":
         searchResultsArray = this.recipesData.filter(
-          recipes =>
-            recipes.name.toLowerCase().includes(pattern) ||
-            recipes.ingredients.some(ingredients =>
+          recipe =>
+            recipe.name.toLowerCase().includes(pattern) ||
+            recipe.ingredients.some(ingredients =>
               ingredients.ingredient.toLowerCase().includes(pattern)
             ) ||
-            recipes.description.includes(pattern)
+            recipe.description.includes(pattern)
         )
 
         break
 
       case "ingredients":
-        searchResultsArray = this.recipesData.filter(recipes =>
-          recipes.ingredients.some(ingredients =>
+        searchResultsArray = this.recipesData.filter(recipe =>
+          recipe.ingredients.some(ingredients =>
             ingredients.ingredient.toLowerCase().includes(pattern)
           )
         )
@@ -81,32 +112,38 @@ export default class Search {
         break
 
       case "appliances":
-        searchResultsArray = this.recipesData.filter(recipes =>
-          recipes.appliance.toLowerCase().includes(pattern)
+        searchResultsArray = this.recipesData.filter(recipe =>
+          recipe.appliance.toLowerCase().includes(pattern)
         )
         break
 
       case "ustensils":
-        searchResultsArray = this.recipesData.filter(recipes =>
-          recipes.ustensils.some(ustensil =>
+        searchResultsArray = this.recipesData.filter(recipe =>
+          recipe.ustensils.some(ustensil =>
             ustensil.toLowerCase().includes(pattern)
           )
         )
         break
 
-      case "tags":
-        searchResultsArray = this.recipesData.filter(
-          recipes =>
-            recipes.ingredients.some(ingredients =>
-              ingredients.ingredient.toLowerCase().includes(pattern)
-            ) ||
-            recipes.appliance.toLowerCase().includes(pattern) ||
-            recipes.ustensils.some(ustensil =>
-              ustensil.toLowerCase().includes(pattern)
-            )
+      case "ingredientsTags":
+        searchResultsArray = this.recipesData.filter(recipe =>
+          recipe.ingredients.some(
+            object => object.ingredient.toLowerCase() === pattern
+          )
         )
 
         break
+
+      case "appliancesTags":
+        searchResultsArray = this.recipesData.filter(
+          recipe => recipe.appliance.toLowerCase() === pattern
+        )
+        break
+
+      case "ustensilsTags":
+        searchResultsArray = this.recipesData.filter(recipe =>
+          recipe.ustensils.some(ustensil => ustensil.toLowerCase() === pattern)
+        )
 
       default:
         break
